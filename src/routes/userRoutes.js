a98a99230025965c10208140b431b5d2823bcbc0
Franklyn Seabra <franklyn.seabra@osf.digital>
2021-02-02 19:35:59 -0300

    fix: Removing wrong route

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,11 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
-const phoneController = require("../controllers/phoneController");
 
 router.get("/", userController.all);
 router.get("/:id", userController.getUserById);
-router.get("/:id/phones", phoneController.getPhoneById);
 router.post("/", userController.insert);
 router.put("/:id", userController.update);
 router.delete("/:id", userController.deleteById);
